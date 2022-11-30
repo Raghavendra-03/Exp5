3a84431926e01599069748c4db2e8f24d6299277
jdpra <jdpra@bobby>
2022-11-30 23:22:59 +0530

    eclipse to sonar

---
+++ src/main/java/project/Exp5/App.java
@@ -0,0 +1,13 @@
+package project.Exp5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
